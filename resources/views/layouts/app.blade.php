7a82b811ef6041a9064078296113953527bc47e0
司文峰 <2535212234@qq.com>
2019-02-28 08:16:52 +0000

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
-
+@yield('styles')
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
@@ -34,6 +34,7 @@
 
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
+  @yield('scripts')
 </body>
 
 </html>
