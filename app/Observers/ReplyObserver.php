3c62691ae3f94bdf75c61d5eb7b814b09739e4d3
司文峰 <2535212234@qq.com>
2019-03-01 07:52:46 +0000

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -6,18 +6,25 @@ use App\Models\Reply;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
+
 use App\Notifications\TopicReplied;
+
 class ReplyObserver
 {
+    public function created(Reply $reply)
+    {
+        $reply->topic->updateReplyCount();
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
+    }
+
     public function creating(Reply $reply)
     {
         $reply->content = clean($reply->content, 'user_topic_body');
     }
-    public function created(Reply $reply)
+
+    public function deleted(Reply $reply)
     {
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
-    	// 通知话题作者有新的评论
-    	$reply->topic->user->notify(new TopicReplied($reply));
+        $reply->topic->updateReplyCount();
     }
 }
