8ce6a8130b531ed89c21ae17515d80f1e6c4e520
司文峰 <2535212234@qq.com>
2019-03-02 01:53:09 +0000

    集成 laravel-permission 扩展包

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -8,18 +8,8 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 class UserPolicy
 {
     use HandlesAuthorization;
-
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function update(User $currentUser, User $user)
     {
-        use HandlesAuthorization;
-        public function update(User $currentUser, User $user)
-        {
-            return $currentUser->id === $user->id;
-        }
+        return $currentUser->id === $user->id;
     }
 }
